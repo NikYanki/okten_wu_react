7e13a3504d8c9aba74ee6d55f1ae129f6692cef5
NikYanki <nankovoj799@gmail.com>
2022-04-08 22:10:14 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 function App() {
-    const click2=()=>console.log('click2')
     return (
         <div>
-            <button onClick={() => console.log("click")}>click me</button>
-            <button onClick={click2}>click me</button>
 
         </div>
     );
