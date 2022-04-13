bc35c64fec755b6380b2927554a8cf56455aaa47
NikYanki <nankovoj799@gmail.com>
2022-04-13 10:42:22 +0200

    fix

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -2,5 +2,4 @@ import axios from 'axios';
 
 import baseURL from '../constants/links';
 
-const axiosService = axios.create({'https://jsonplaceholder.typicode.com'});
-export default axiosService
+export const axiosService = axios.create({baseURL:'https://jsonplaceholder.typicode.com'});
