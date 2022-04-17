2e7512b4cc7dabcc1a8002005f76e2ca65fca13c
NikYanki <nankovoj799@gmail.com>
2022-04-17 21:53:46 +0200

    fix

--- src/validators/CarValidators.js
+++ src/validators/CarValidators.js
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
-const CarValidators = Joi.object({
+export const CarValidators = Joi.object({
     model:Joi.string().min(2).max(20).required().messages({
         'string.empty':'Model не може бути пустим',
         'string.min':'Model має буті мінімум 2 символи'
@@ -11,7 +11,3 @@ const CarValidators = Joi.object({
     price: Joi.number().min(0).max(1000000).required(),
     year: Joi.number().min(1990).max(new Date().getFullYear()).required()
 });
-
-export {
-    CarValidators
-}
